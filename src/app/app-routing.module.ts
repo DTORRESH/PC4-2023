479a7deec4db6ff0804dd9de29e841b5490ba627
DTORRESH <DJ.TORRESH@GMAIL.COM>
2023-11-20 17:23:11 -0500

    subir PC4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CursosComponent } from './gestion/cursos/cursos.component';
+import { AlumnosComponent } from './gestion/alumnos/alumnos.component';
+import { MenuComponent } from './gestion/menu/menu.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'cursos', component: CursosComponent },
+  { path: 'alumnos', component: AlumnosComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Alumno } from './gestion/alumnos/alumno';
+import { ServicioAlumnosService } from './gestion/alumnos/servicio-alumnos.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'route';
+  title = 'Mantenimiento ';
+   constructor(private miServicio:ServicioAlumnosService){}
+  alumnos:Alumno[]=[
+    new Alumno("a01","Maria","Perez","29/10/1990","Los pinos 100","mPerez@gmail.com","999 999 999"),
+    new Alumno("a02","Juan","Perez","29/10/1990","Los pinos 100","mPerez@gmail.com","999 999 999"),
+    new Alumno("a03","Pedro","Perez","29/10/1990","Los pinos 100","mPerez@gmail.com","999 999 999"),
+    new Alumno("a04","Oscar","Perez","29/10/1990","Los pinos 100","mPerez@gmail.com","999 999 999"),
+
+  ];
+  agregarAlumno(){
+    let miAlumno= new Alumno(this.cuadroId, this.cuadroNombres,this.cuadroApellidos,this.cuadroFechaNacimiento,this.cuadroCorreo,this.cuadroTelefono);
+    this.miServicio.muestraMensaje("Nombre del Alumno"+miAlumno.nombres);
+    this.alumnos.push(miAlumno);
+
+  }
+cuadroId:string="";
+cuadroNombres:string="";
+cuadroApellidos:string="";
+cuadroFechaNacimiento:string="";
+cuadroCorreo:string="";
+cuadroTelefono:string="";
+
 }
